20db6fd6a283c729f030b35923519e3aad698620
jin <zjaxjrhkd100@gmail.com>
2025-02-17 21:40:32 +0900

    Init

---
+++ Assets/Resource/Script/Furniture/Bed.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bed : Monob
+{
+    public override void Use()
+    {
+        Debug.Log("Ä§´ë");
+    }
+}
