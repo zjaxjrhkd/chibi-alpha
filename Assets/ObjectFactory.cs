39286293d477db132be3e4bbbb4e2687f86e8fe3
jin <zjaxjrhkd100@gmail.com>
2025-02-19 22:11:43 +0900

    [feat] <상점 구매기능과 오브젝트 상태 만들기> (#5)
    상점에서 그림을 클릭하면 가구 오브젝트 생성
    오브젝트의 상태를 설정해서 콜라이더가 겹치는지 확인
    
    가구 오브젝ㅌ그 생성 스크립트 작성
    오브젝트 상태 구현
    
    Close #5

---
+++ Assets/ObjectFactory.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFactory : MonoBehaviour
+{
+    public GameObject CreateFurniture(GameObject furniturePrefab)
+    {
+        Vector3 spawnPosition = new Vector3(0, 0, 0); // 원하는 위치
+        GameObject newFurniture = Instantiate(furniturePrefab, spawnPosition, Quaternion.identity);
+        return newFurniture; // 생성된 오브젝트 반환
+    }
+
+}
--- Assets/Resource/Script/Shop.cs
+++ Assets/Resource/Script/Shop.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Shop : MonoBehaviour
 {
-    public List<GameObject> furnityreList = new List<GameObject>();
+    public List<GameObject> furnitureList = new List<GameObject>();
 
 
     // Start is called before the first frame update
