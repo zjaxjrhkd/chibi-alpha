4eb991b8a53499007225e909c07179ae782431a8
jin <zjaxjrhkd100@gmail.com>
2025-02-24 21:58:56 +0900

    [fix] 캐릭터 메뉴 버튼 변경
    fix : 메뉴버튼 작동방식 변경
    
    캐릭터 의상변경 메뉴 작동방식 수정
    GameManager에서 의상변경하도록 수정
    
    Close #13

--- Assets/Resource/Script/ChangeClothes.cs
+++ Assets/Resource/Script/ChangeClothes.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // UI 관련 기능 사용을 위해 추가
 
 public class ChangeClothes : MonoBehaviour
 {
     public List<GameObject> clothesGroup;
 
+
+    void Start()
+    {
+
+    }
+
     public void ChangeClothesOne()
     {
-        Debug.Log("첫번쨰");
+        Debug.Log("첫번째");
         // 첫 번째 옷을 활성화하고 나머지는 비활성화
         for (int i = 0; i < clothesGroup.Count; i++)
         {
@@ -18,7 +25,7 @@ public class ChangeClothes : MonoBehaviour
 
     public void ChangeClothesTwo()
     {
-        Debug.Log("두번쨰");
+        Debug.Log("두번째");
         // 두 번째 옷을 활성화하고 나머지는 비활성화
         for (int i = 0; i < clothesGroup.Count; i++)
         {
@@ -28,7 +35,7 @@ public class ChangeClothes : MonoBehaviour
 
     public void ChangeClothesThree()
     {
-        Debug.Log("세번쨰");
+        Debug.Log("세번째");
         // 세 번째 옷을 활성화하고 나머지는 비활성화
         for (int i = 0; i < clothesGroup.Count; i++)
         {
